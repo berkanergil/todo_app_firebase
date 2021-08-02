343dab761a5072d6218bb0e8ed04a9d2e8f96ac2
berkanergil <69623905+berkanergil@users.noreply.github.com>
2021-08-02 18:17:56 +0300

    CRUD operations done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
@@ -20,11 +24,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List todos = new List.generate(5, (int index) => "$index. item");
-  String input = "";
+  final titleController= new TextEditingController();
+  final subTitleController= new TextEditingController();
+  final titleEditController= new TextEditingController();
+  final subTitleEditController= new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> _todosStream = FirebaseFirestore.instance.collection('todos').orderBy("created_at",descending: true).snapshots();
+    CollectionReference todos= FirebaseFirestore.instance.collection("todos");
     return Scaffold(
       appBar: AppBar(
         title: Text("To Do List"),
@@ -39,19 +47,39 @@ class _MyAppState extends State<MyApp> {
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8)),
                   title: Text("Add Todo"),
-                  content: TextField(
-                    minLines: 1,
-                    maxLines: 5,
-                    onChanged: (String value) {
-                      input = value;
-                    },
+                  content: Column(
+                    children: [
+                      TextField(
+                        controller: titleController,
+                        minLines: 1,
+                        maxLines: 5,
+                        decoration: InputDecoration(
+                            labelText: "Title"
+                        ),
+                      ),
+                      TextField(
+                        controller: subTitleController,
+                        minLines: 1,
+                        maxLines: 5,
+                        decoration: InputDecoration(
+                            labelText: "SubTitle"
+                        ),
+                      ),
+                    ],
                   ),
+
                   actions: [
                     TextButton(
                         onPressed: () {
                           setState(() {
-                            todos.add(input);
+                            todos.add({
+                              "title":titleController.text,
+                              "subtitle":subTitleController.text,
+                              "created_at":FieldValue.serverTimestamp(),
+                            });
                           });
+                          titleController.clear();
+                          subTitleController.clear();
                           Navigator.of(context).pop();
                         },
                         child: Text("Add"))
@@ -64,35 +92,92 @@ class _MyAppState extends State<MyApp> {
           color: Colors.white,
         ),
       ),
-      body: SafeArea(
-        child: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            return Dismissible(
-                key: Key(todos[index]),
-                child: Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)
-                  ),
-                  child: ListTile(
-                    title: Text(todos[index]),
-                    trailing: IconButton(
-                      icon: Icon(
-                        Icons.delete,
-                        color: Theme.of(context).accentColor,
-                      ),
-                      onPressed: (){
-                        setState(() {
-                          todos.removeAt(index);
-                        });
-                      },
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _todosStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+          return new ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot todo) {
+              return Dismissible(
+                  key: Key(todo["title"]),
+                  child: Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)
                     ),
-                  ),
-                ));
-          },
-          itemCount: todos.length,
-        ),
-      ),
+                    child: InkWell(
+                      onTap: (){
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                                title: Text("Edit Todo"),
+                                content: Column(
+                                  children: [
+                                    TextField(
+                                      controller: titleEditController..text=(todo["title"]),
+                                      minLines: 1,
+                                      maxLines: 5,
+                                      decoration: InputDecoration(
+                                          labelText: "Title"
+                                      ),
+                                    ),
+                                    TextField(
+                                      controller: subTitleEditController..text=(todo["subtitle"]),
+                                      minLines: 1,
+                                      maxLines: 5,
+                                      decoration: InputDecoration(
+                                          labelText: "SubTitle"
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          todo.reference.update({
+                                            "title":titleEditController.text,
+                                            "subtitle":subTitleEditController.text,
+                                            "created_at":FieldValue.serverTimestamp(),
+                                          });
+                                        });
+                                        titleEditController.clear();
+                                        subTitleEditController.clear();
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text("Update"))
+                                ],
+                              );
+                            });
+                      },
+                      child: ListTile(
+                        title: Text(todo["title"]),
+                        trailing: IconButton(
+                          icon: Icon(
+                            Icons.delete,
+                            color: Theme.of(context).accentColor,
+                          ),
+                          onPressed: (){
+                            setState(() {
+                                todo.reference.delete();
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ));
+            }).toList(),
+          );
+        },
+      )
     );
   }
 }
